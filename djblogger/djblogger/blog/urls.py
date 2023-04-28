d4dbe3520d57bcb62837c5d0aaa50bd76a462316
Mikhail <110410802+Lunyakin@users.noreply.github.com>
2023-04-28 21:11:57 +0300

    nc

--- djblogger/djblogger/blog/urls.py
+++ djblogger/djblogger/blog/urls.py
@@ -2,5 +2,6 @@ from djblogger.blog.views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='homepage')
+    path('', HomeView.as_view(), name='homepage'),
+    path('<slug:post>/', post_single, name='post_single')
 ]
