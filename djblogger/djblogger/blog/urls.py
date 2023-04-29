a048da3868c543ee119339a66873995f7d6a7194
Mikhail <110410802+Lunyakin@users.noreply.github.com>
2023-04-29 14:54:25 +0300

    nc

--- djblogger/djblogger/blog/urls.py
+++ djblogger/djblogger/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', HomeView.as_view(), name='homepage'),
-    path('<slug:post>/', post_single, name='post_single')
+    path('<slug:post>/', post_single, name='post_single'),
+    path('tag/<slug:tag>/', TagListView.as_view(), name='post_by_tag')
 ]
